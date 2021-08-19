89ad10181dbc63d4019b3f100c5eb07f195f475d
seotrot <usmaniseo@gmail.com>
2021-08-19 22:12:11 +0500

    first

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import NavBar from '../components/NavBar'
+import Results from '../components/Results'
+import requests from '../utils/requests'
+
+export default function Home({ results }) {
+  return (
+    <div>
+      <Head>
+        <title>Hulu clone</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+      <NavBar />
+      <Results results={results} />
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json())
+  return {
+    props: {
+      results: request.results,
+    },
+  }
+}
